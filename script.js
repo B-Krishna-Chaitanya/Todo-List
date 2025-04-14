e78c0d864e45a6c9ee44ff839dcf529596ab4155
B-Krishna-Chaitanya <srichaitanyabkc01@gmail.com>
2025-04-14 23:58:28 +0530

    version 2

--- script.js
+++ script.js
@@ -1,40 +1,36 @@
-const todoList = [];
+const todoList =JSON.parse(localStorage.getItem('task')) || [];
 
 function renderTodoList() {
   let todoListHTML = ''; 
 
-  todoList.forEach(function(todoObject, index) {
-    const { name, dueDate } = todoObject;
-    const html = `
-      <div>${name}</div>
-      <div> ${dueDate}</div> 
-      <button onclick="
-          todoList.splice(${index},1);
-          renderTodoList();
-        " class="delete-todo-button">Delete
-      </button>  
-    `;
-    todoListHTML += html;
-  }
-  );
-  /*
   for (let i=0; i < todoList.length; i++) {
     const todoObject = todoList[i];
     const { name, dueDate } = todoObject;
     const html = `
       <div>${name}</div>
       <div> ${dueDate}</div> 
-      <button onclick="
-          todoList.splice(${i},1);
-          renderTodoList();
-        " class="delete-todo-button">Delete
+      <button class="delete-todo-button js-delete-todo-button">Delete
       </button>  
     `;
     todoListHTML += html;
   }
-  */
   document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+
+  document.querySelectorAll('.js-delete-todo-button').forEach((deleteButton,index) => {
+    deleteButton.addEventListener('click',() => {
+      todoList.splice(index,1);
+      renderTodoList();
+    })
+  });
 }
+
+//instead of onclick attribute in html use addEventListener
+document.querySelector('.js-add-todo-button').addEventListener('click',() => {
+  addToDo();
+});
+
+//to display to do list just after opening the webpage i.e., already existing tasks
+renderTodoList();
 
 function addToDo() {
   const nameInputElement = document.querySelector('.js-name-input');
@@ -43,9 +39,17 @@ function addToDo() {
   const dueDateInputElement = document.querySelector('.js-dueDate-input');
   const dueDate = dueDateInputElement.value;
 
-  todoList.push({name, dueDate}); //used shorthand property of objects
+  const object = {name, dueDate}; //used shorthand property of objects
+
+  todoList.push(object); 
+
+  saveToStorage();
 
   nameInputElement.value = null; // or inputElement.value = '';
 
   renderTodoList();
+}
+
+function saveToStorage() {
+  localStorage.setItem('task', JSON.stringify(todoList));
 }
